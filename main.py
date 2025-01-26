3a5390e1b036ca963c2d69f5fe8735491142f4e1
Dimonov19 <56956068+Dimonov19@users.noreply.github.com>
2025-01-26 05:35:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,4 +42,5 @@ def generate():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
